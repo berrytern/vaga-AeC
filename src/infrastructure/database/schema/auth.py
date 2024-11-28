acffade8f2cc95b6611c6764abacce99b06e5d3a
berrytern <berrytern@gmail.com>
2024-11-28 19:01:22 -0300

    chore: rename folder's name from postgre to database

--- src/infrastructure/database/schema/auth.py
+++ src/infrastructure/database/schema/auth.py
@@ -7,8 +7,7 @@ from . import Base
 class Auth(Base):
     __tablename__ = "auth"
 
-    id = Column(UUID(as_uuid=True), primary_key=True,
-                default=uuid.uuid4, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     username = Column(String(30), unique=True, nullable=False, index=True)
     user_type = Column(String(30), nullable=False)
     password = Column(String)
@@ -17,5 +16,7 @@ class Auth(Base):
     foreign_id = Column(UUID, nullable=True)
 
     def __repr__(self) -> str:
-        return f"Auth(id={self.id!r}, username={self.username!r}" \
+        return (
+            f"Auth(id={self.id!r}, username={self.username!r}"
             + f", user_type={self.user_type}, last_login={self.last_login}, foreign_id={self.foreign_id})"
+        )
