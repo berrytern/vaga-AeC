745f99ffaa82f31cec2ea9e47e14a19c904e8c25
berrytern <berrytern@gmail.com>
2024-11-28 19:43:30 -0300

    feat: add CORS middleware and authentication router to the FastAPI application

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
+from src.main.routes import AUTH_ROUTER
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -6,3 +8,15 @@ app = FastAPI(
     version="1.0",
     description="A simple CRUD API for managing books in a bookstore.",
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+app.include_router(AUTH_ROUTER, prefix="/auth", tags=["auth"])
