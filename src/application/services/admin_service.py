ef04ac44121578ede9d0a11450c9909474df1438
berrytern <berrytern@gmail.com>
2024-12-01 22:29:11 -0300

    feat: implement AdminService for managing admin users with CRUD operations

---
+++ src/application/services/admin_service.py
@@ -0,0 +1,61 @@
+from typing import Optional, Dict, List, Any
+from src.application.models import (
+    AuthModel,
+    AdminQueryModel,
+    AdminModel,
+    CreateAdminModel,
+)
+from src.application.utils import UserTypes
+from src.infrastructure.repositories import AuthRepository, AdminRepository
+import bcrypt
+
+
+class AdminService:
+    def __init__(self, repository: AdminRepository, auth_repository: AuthRepository):
+        self.repository = repository
+        self.auth_repository = auth_repository
+
+    async def create(self, admin: CreateAdminModel) -> Dict[str, Any]:
+        result = await self.repository.create(
+            admin.model_dump(
+                exclude_none=True,
+                exclude={"id", "username", "password"},
+            ),
+            commit=False,
+        )
+        admin.password = bcrypt.hashpw(
+            admin.password.encode(), bcrypt.gensalt(13)
+        ).decode()
+        auth = AuthModel(
+            **{
+                **admin.model_dump(exclude_none=True, exclude={"id"}),
+                **{
+                    "user_type": UserTypes.ADMIN.value,
+                    "refresh_token": None,
+                    "last_login": None,
+                    "foreign_id": str(result["id"]),
+                },
+            }
+        )
+
+        return await self.auth_repository.create(
+            auth.model_dump(exclude_none=True), True
+        )
+        """except BaseException:
+            await session.rollback()
+            raise ConflictException(
+                "Already exist",
+                "email/username already used",
+            )"""
+
+    async def get_one(self, admin_id: str):
+        return await self.repository.get_one({"id": admin_id})
+
+    async def get_all(self, query: AdminQueryModel) -> List[Dict[str, Any]]:
+        return await self.repository.get_all(query)
+
+    async def update_one(self, admin: AdminModel) -> Optional[Any]:
+        return await self.repository.update_one(id, admin.model_dump(exclude_none=True))
+
+    async def delete_one(self, admin_id: str) -> None:
+        await self.repository.delete_one(admin_id)
