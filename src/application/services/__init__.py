cb54a559639053703ff1e857cd5a270d27681b71
berrytern <berrytern@gmail.com>
2024-12-01 23:33:32 -0300

    feat: add ReaderService for managing reader operations and update AdminService to exclude password in model dumps

--- src/application/services/__init__.py
+++ src/application/services/__init__.py
@@ -1,1 +1,2 @@
 from .admin_service import AdminService
+from .reader_service import ReaderService
--- src/application/services/reader_service.py
+++ src/application/services/reader_service.py
@@ -1,27 +1,27 @@
 from typing import Optional, Dict, List, Any
 from src.application.domain.models import (
     AuthModel,
-    AdminQueryModel,
-    AdminModel,
-    CreateAdminModel,
+    ReaderQueryModel,
+    UpdateReaderModel,
+    CreateReaderModel,
 )
 from src.application.domain.utils import UserTypes
-from src.infrastructure.repositories import AuthRepository, AdminRepository
+from src.infrastructure.repositories import AuthRepository, ReaderRepository
 import bcrypt
 
 
-class AdminService:
-    def __init__(self, repository: AdminRepository, auth_repository: AuthRepository):
+class ReaderService:
+    def __init__(self, repository: ReaderRepository, auth_repository: AuthRepository):
         self.repository = repository
         self.auth_repository = auth_repository
 
-    async def create(self, admin: CreateAdminModel) -> Dict[str, Any]:
+    async def create(self, admin: CreateReaderModel) -> Dict[str, Any]:
         result = await self.repository.create(
             admin.model_dump(
                 exclude_none=True,
                 exclude={"id", "password"},
             ),
-            commit=False,
+            commit=True,
         )
         admin.password = bcrypt.hashpw(
             admin.password.encode(), bcrypt.gensalt(13)
@@ -45,10 +45,10 @@ class AdminService:
     async def get_one(self, admin_id: str):
         return await self.repository.get_one({"id": admin_id})
 
-    async def get_all(self, query: AdminQueryModel) -> List[Dict[str, Any]]:
+    async def get_all(self, query: ReaderQueryModel) -> List[Dict[str, Any]]:
         return await self.repository.get_all(query)
 
-    async def update_one(self, admin: AdminModel) -> Optional[Any]:
+    async def update_one(self, admin: UpdateReaderModel) -> Optional[Any]:
         return await self.repository.update_one(id, admin.model_dump(exclude_none=True))
 
     async def delete_one(self, admin_id: str) -> None:
