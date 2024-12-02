9e74604589364360b5a34692bd281f5e20afb6c3
berrytern <berrytern@gmail.com>
2024-12-01 23:33:52 -0300

    feat: add ReaderController for managing reader operations and integrate with ReaderService

--- src/presenters/controllers/__init__.py
+++ src/presenters/controllers/__init__.py
@@ -1,2 +1,3 @@
 from .admin_controller import AdminController
 from .auth_controller import AuthController
+from .reader_controller import ReaderController
---
+++ src/presenters/controllers/reader_controller.py
@@ -0,0 +1,31 @@
+from src.application.domain.models import (
+    CreateReaderModel,
+    UpdateReaderModel,
+    ReaderQueryModel,
+)
+from src.application.services import ReaderService
+
+
+class ReaderController:
+    def __init__(self, service: ReaderService):
+        self.service = service
+
+    async def create(self, admin: CreateReaderModel):
+        result = await self.service.create(admin)
+        return result, 201, {}
+
+    async def get_one(self, admin_id: str):
+        result = await self.service.get_one(admin_id)
+        return result, 200, {}
+
+    async def get_all(self, query: ReaderQueryModel):
+        result = await self.service.get_all(query)
+        return result, 200, {}
+
+    async def update_one(self, admin: UpdateReaderModel):
+        result = await self.service.update_one(admin)
+        return result, 200, {}
+
+    async def delete_one(self, admin_id: str):
+        await self.service.delete_one(admin_id)
+        return None, 200, {}
