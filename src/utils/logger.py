856606f01ce0b91c97e0e5c15782f8591df48e2f
berrytern <berrytern@gmail.com>
2024-12-02 19:14:50 -0300

    refactor: remove CsvLogger class and related functions to simplify logging utility

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,57 +1,5 @@
-from typing import Any
-from datetime import datetime
 from logging.handlers import RotatingFileHandler
-from threading import Lock
-from os.path import isfile
 import logging
-import csv
-import io
-
-
-class CsvLogger:
-    def __init__(self, filename: str, headers: str) -> None:
-        self.filename = filename
-        self.lock = Lock()
-        self.output_io = io.StringIO()
-        self.writer_io = csv.writer(
-            self.output_io, delimiter=";", quoting=csv.QUOTE_ALL
-        )
-        self.wheaders(headers)
-
-    def wheaders(self, headers: str) -> None:
-        if not isfile(self.filename):
-            with open(self.filename, "a") as file:
-                file.write(headers + "\n")
-
-    def acquire(self) -> None:
-        self.lock.acquire()
-
-    def release(self) -> None:
-        self.lock.release()
-
-    def info(self, record: Any) -> None:
-        self.acquire()
-        with open(self.filename, "a") as file:
-            if isinstance(record, list):
-                list(map(lambda x: file.write(self.format(x) + "\n"), record))
-            else:
-                file.write(self.format(record) + "\n")
-        self.release()
-
-    def format(self, record: Any) -> str:
-        date = datetime.today().strftime("%Y-%m-%d , %H:%M:%S")
-        if isinstance(record, dict):
-            self.writer_io.writerow([date] + list(record.values()))
-        elif isinstance(record, list):
-            self.writer_io.writerow([date] + record)
-        data = self.output_io.getvalue()
-        self.output_io.truncate(0)
-        self.output_io.seek(0)
-        return data.strip()
-
-
-def get_csv_logger(log_filename: str, headers: str) -> CsvLogger:
-    return CsvLogger(log_filename, headers)
 
 
 FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
