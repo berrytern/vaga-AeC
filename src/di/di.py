a1963c7dcc1cc677313c472aeddf1a6e91eb5a37
berrytern <berrytern@gmail.com>
2024-12-06 07:15:25 -0300

    feat: refactor dependency injection for favorite controller in routes

--- src/di/di.py
+++ src/di/di.py
@@ -1,8 +1,23 @@
-from src.application.domain.models import ReaderModel, ReaderList
-from src.application.services.reader_service import ReaderService
-from src.infrastructure.database.schemas import ReaderSchema
-from src.infrastructure.repositories import AuthRepository, ReaderRepository
-from src.presenters.controllers.reader_controller import ReaderController
+from src.application.domain.models import (
+    FavoriteModel,
+    FavoriteList,
+    ReaderModel,
+    ReaderList,
+)
+from src.application.services import FavoriteService, ReaderService
+from src.infrastructure.database.schemas import (
+    FavoriteBookSchema,
+    ReaderSchema,
+)
+from src.infrastructure.repositories import (
+    AuthRepository,
+    FavoriteRepository,
+    ReaderRepository,
+)
+from src.presenters.controllers import (
+    ReaderController,
+    FavoriteController,
+)
 
 
 class DI:
@@ -12,3 +27,18 @@ class DI:
         auth_repository = AuthRepository(db_session)
         service = ReaderService(repository, auth_repository)
         return ReaderController(service)
+
+    @classmethod
+    def reader_favorite_controller(cls, db_session) -> FavoriteController:
+        reader_repository = ReaderRepository(
+            db_session, ReaderSchema, ReaderModel, ReaderList
+        )
+        repository = FavoriteRepository(
+            db_session,
+            FavoriteBookSchema,
+            FavoriteModel,
+            FavoriteList,
+            reader_repository,
+        )
+        service = FavoriteService(repository)
+        return FavoriteController(service)
--- src/main/routes/reader_favorite_route.py
+++ src/main/routes/reader_favorite_route.py
@@ -4,13 +4,8 @@ from src.application.domain.models import (
     FavoriteModel,
     FavoriteList,
     FavoriteQueryModel,
-    ReaderModel,
-    ReaderList,
 )
-from src.application.services import FavoriteService
-from src.infrastructure.database.schemas import FavoriteBookSchema, ReaderSchema
-from src.infrastructure.repositories import FavoriteRepository, ReaderRepository
-from src.presenters.controllers import FavoriteController
+from src.di import DI
 from src.main.middlewares import auth_middleware, session_middleware
 
 READER_FAVORITE_ROUTER = APIRouter()
@@ -22,18 +17,9 @@ READER_FAVORITE_ROUTER = APIRouter()
 @auth_middleware("bkf:c")
 @session_middleware
 async def set_book_as_favorite(request: Request, reader_id: str, book_id: str):
-    reader_repository = ReaderRepository(
-        request.state.db_session, ReaderSchema, ReaderModel, ReaderList
+    response = await DI.reader_favorite_controller(request.state.db_session).create(
+        reader_id, book_id
     )
-    repository = FavoriteRepository(
-        request.state.db_session,
-        FavoriteBookSchema,
-        FavoriteModel,
-        FavoriteList,
-        reader_repository,
-    )
-    service = FavoriteService(repository)
-    response = await FavoriteController(service).create(reader_id, book_id)
     return JSONResponse(
         content=response[0], status_code=response[1], headers=response[2]
     )
@@ -46,18 +32,9 @@ async def get_all_favorite_books_of_reader(request: Request, reader_id: str):
     query_params = dict(request.query_params)
     query = FavoriteQueryModel(**{**query_params, "reader_id": reader_id}).query_dict()
 
-    reader_repository = ReaderRepository(
-        request.state.db_session, ReaderSchema, ReaderModel, ReaderList
+    response = await DI.reader_favorite_controller(request.state.db_session).get_all(
+        query
     )
-    repository = FavoriteRepository(
-        request.state.db_session,
-        FavoriteBookSchema,
-        FavoriteModel,
-        FavoriteList,
-        reader_repository,
-    )
-    service = FavoriteService(repository)
-    response = await FavoriteController(service).get_all(query)
     return JSONResponse(
         content=response[0], status_code=response[1], headers=response[2]
     )
@@ -72,18 +49,9 @@ async def get_one_favorite_book_of_reader(
     request: Request, reader_id: str, book_id: str
 ):
 
-    reader_repository = ReaderRepository(
-        request.state.db_session, ReaderSchema, ReaderModel, ReaderList
+    response = await DI.reader_favorite_controller(request.state.db_session).get_one(
+        reader_id, book_id
     )
-    repository = FavoriteRepository(
-        request.state.db_session,
-        FavoriteBookSchema,
-        FavoriteModel,
-        FavoriteList,
-        reader_repository,
-    )
-    service = FavoriteService(repository)
-    response = await FavoriteController(service).get_one(reader_id, book_id)
     return JSONResponse(
         content=response[0], status_code=response[1], headers=response[2]
     )
@@ -96,18 +64,9 @@ async def delete_one_favorite_book_of_reader(
     request: Request, reader_id: str, book_id: str
 ):
 
-    reader_repository = ReaderRepository(
-        request.state.db_session, ReaderSchema, ReaderModel, ReaderList
+    response = await DI.reader_favorite_controller(request.state.db_session).delete_one(
+        reader_id, book_id
     )
-    repository = FavoriteRepository(
-        request.state.db_session,
-        FavoriteBookSchema,
-        FavoriteModel,
-        FavoriteList,
-        reader_repository,
-    )
-    service = FavoriteService(repository)
-    response = await FavoriteController(service).delete_one(reader_id, book_id)
     return JSONResponse(
         content=response[0], status_code=response[1], headers=response[2]
     )
