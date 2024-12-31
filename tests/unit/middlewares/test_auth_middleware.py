c0f51581b354bb84bfb999d4b317d2efdc0a4cb0
berrytern <berrytern@gmail.com>
2024-12-31 09:35:02 -0600

    test: add unit tests for auth middleware ownership validation

--- tests/unit/middlewares/test_auth_middleware.py
+++ tests/unit/middlewares/test_auth_middleware.py
@@ -212,3 +212,67 @@ async def test_auth_middleware_without_authorization_header(request_mock):
         # Test failure cases
         assert exc_info.value.status_code == 401
         assert exc_info.value.detail == "Authorization header missing"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "payload,scope,authorized",
+    [
+        (
+            ADMIN_PAYLOAD,
+            ADMIN_SCOPE[0],
+            True,
+        ),
+        (
+            READER_PAYLOAD,
+            READER_SCOPE[0],
+            False,
+        ),
+    ],
+)
+async def test_auth_middleware_not_owned(
+    request_mock, payload, scope, authorized: bool
+):
+    id_key = str(uuid4())
+    id_value = str(uuid4())
+    jwt_secret = str(uuid4())  # Create a random jwt secret
+    token = jwt.encode(payload, "secret")
+    # Setup request mock
+    request_mock.headers.get = MagicMock(return_value=token)
+    redis_mock = Mock()
+    redis_mock.get = AsyncMock(return_value=None)
+
+    expected_response = str(uuid4())  # Create a random string
+
+    # Create mock for next function
+    async def next_mock(*args, request: Request, **kwargs):
+        return expected_response
+
+    with patch("src.main.middlewares.auth.RedisClient", redis_mock), patch(
+        "src.main.middlewares.auth.settings.JWT_SECRET", jwt_secret
+    ), patch(
+        "src.main.middlewares.auth.jwt.decode", return_value=payload
+    ) as decode_mock:
+        assert callable(auth_middleware)
+        wrapper = auth_middleware(scope, id_key)
+        assert callable(wrapper)
+        assert callable(wrapper(next_mock))
+        middleware = wrapper(next_mock)
+        if authorized:
+            response = await middleware(request=request_mock, **{id_key: id_value})
+            assert response == expected_response
+            request_mock.headers.get.assert_called_with("Authorization")
+            decode_mock.assert_called_with(
+                token, jwt_secret, algorithms=["HS256"], verify=True
+            )
+            redis_mock.get.assert_called_with(token)
+
+            # Test success cases
+            assert hasattr(request_mock.state, "user")
+            assert request_mock.state.user == payload
+        else:
+            with pytest.raises(HTTPException) as exc_info:
+                await middleware(request=request_mock, **{id_key: id_value})
+            # Test failure cases
+            assert exc_info.value.status_code == 401
+            assert exc_info.value.detail == "Resource not owned by user"
